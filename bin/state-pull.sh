b35aa028ea275a76dad66dd09553be5038ac1874
Bryan Jiencke <bryan.jiencke@gmail.com>
2021-11-27 23:03:47 +0000

    WIP

--- bin/state-pull.sh
+++ bin/state-pull.sh
@@ -3,8 +3,9 @@
 set -e
 
 cd /app/docroot
-drush sql-sync @sysf.ide1 @self --create-db -y -v
-drush rsync @sysf.ide1:sites/default/files/ @self:sites/default/files/ -y -v
+drush sql-sync @sysf.prd @self --create-db -y -v
+drush rsync @sysf.prd:sites/default/files/ @self:sites/default/files/ -y -v
+drush rsync @sysf.prd:/app/files-private/ @self:/app/files-private/ -y -v
 drush cr -v
 drush uli -v
 
