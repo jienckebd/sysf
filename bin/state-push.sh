05edf604bdf452ae7e0173e77884da1cf601aeb3
Bryan Jiencke <bryan.jiencke@gmail.com>
2022-02-20 14:56:21 +0000

    WIP

--- bin/state-push.sh
+++ bin/state-push.sh
@@ -2,24 +2,20 @@
 
 set -e
 
-# drush sql-sync @self @sysf.dev --target-dump=/app/files-private/sync.sql.gz --create-db -y -v
-# drush rsync @self:sites/default/files/ @sysf.dev:sites/default/files/ -y -v
-# drush @sysf.dev cr -v
-# drush @sysf.dev uli -v
 
-# drush sql-sync @self @sysf.stg --target-dump=/app/files-private/sync.sql.gz --create-db -y -v
-# drush rsync @self:sites/default/files/ @sysf.stg:sites/default/files/ -y -v
-# drush @sysf.stg cr -v
-# drush @sysf.stg uli -v
+# drush sql-sync @self @sysf.prd --target-dump=/app/files-private/sync.sql.gz --create-db -y -v
+# drush rsync @self:/app/files-private/ @sysf.prd:/app/files-private/ -y -v
+# drush @sysf.prd cr -v
+# drush @sysf.prd uli -v
 
-drush sql-sync @self @sysf.prd --target-dump=/app/files-private/sync.sql.gz --create-db -y -v
-drush rsync @self:/app/files-private/ @sysf.prd:/app/files-private/ -y -v
-drush @sysf.prd cr -v
-drush @sysf.prd uli -v
+drush sql-sync @self @sysf.dev --target-dump=/app/files-private/sync.sql.gz --create-db -y -v
+drush rsync @self:sites/default/files/ @sysf.dev:sites/default/files/ -y -v
+drush @sysf.dev cr -v
+drush @sysf.dev uli -v
 
-# drush sql-sync @self @sysf.ide1 --target-dump=/app/files-private/sync.sql.gz --create-db -y -v
-# drush rsync @self:sites/default/files/ @sysf.ide1:sites/default/files/ -y -v
-# drush @sysf.ide1 cr -v
-# drush @sysf.ide1 uli -v
+drush sql-sync @self @sysf.stg --target-dump=/app/files-private/sync.sql.gz --create-db -y -v
+drush rsync @self:sites/default/files/ @sysf.stg:sites/default/files/ -y -v
+drush @sysf.stg cr -v
+drush @sysf.stg uli -v
 
 echo "Done."
