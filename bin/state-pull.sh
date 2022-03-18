1d62f59e0d34fa5f364771d958dd728f2e4f6f49
Bryan Jiencke <bryan.jiencke@gmail.com>
2022-03-18 20:41:37 +0000

    WIP

--- bin/state-pull.sh
+++ bin/state-pull.sh
@@ -7,6 +7,8 @@ drush sql-sync @sysf.prd @self --create-db -y -v
 drush rsync @sysf.prd:sites/default/files/ @self:sites/default/files/ -y -v
 drush rsync @sysf.prd:/app/files-private/ @self:/app/files-private/ -y -v
 drush cr -v
+drush search-api:reset-tracker -v
+drush search-api:index -v
 drush uli -v
 
 echo "Done."
